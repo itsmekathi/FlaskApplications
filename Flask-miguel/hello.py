d4f021423692cb4a687d693867546296d7dccf24
kathiravan B <itsmekathi92@gmail.com>
2019-10-06 19:41:16 +0530

    Chapter-2 lessons

--- Flask-miguel/hello.py
+++ Flask-miguel/hello.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, url_for, abort
+from flask_script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
+def home():
+    user_agent = request.headers.get('User-Agent')
+    response = make_response(f'<h1>Hello World!</h1><h2>Your user agent is:{user_agent}</h2>')
+    response.set_cookie('answer','42')
+    response.set_cookie('application_name','Home Management system')
+    return response
+
+@app.route('/index')
 def index():
-    return '<h1>Hello World!</h1>'
+    return redirect(url_for('home'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -13,6 +24,9 @@ def user(name):
 def user_by_id(id):
     return f'<h1>User Id is :{id}</h1>'
 
+@app.route('/error')
+def error():
+    return '<h1>Bad request</h1>', 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
