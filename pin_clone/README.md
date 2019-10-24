65d1880dd0cc7ec8016e68aaae73e898c81f1562
kathiravan B <itsmekathi92@gmail.com>
2019-10-24 11:18:02 +0530

    Added code for pin_clone

---
+++ pin_clone/README.md
@@ -0,0 +1,9 @@
+# pin_clone example
+Contains code for pin clone
+
+# Libraries used
+Flask
+angularjs
+
+# Video link
+https://www.youtube.com/watch?v=2geC50roans
---
+++ pin_clone/pin_clone.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, Text
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pin.db'
+db = SQLAlchemy(app)
+
+
+class Pin(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, unique=False)
+    image = Column(Text, unique=False)
+
+
+db.create_all()
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+app.debug = True
+
+if __name__ == '__main__':
+    app.run()
